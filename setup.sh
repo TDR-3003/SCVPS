3c8c0404c4e71fe9662ae5e76c60947d476bb632
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-06 22:51:21 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -77,13 +77,12 @@ wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ohp/ohp.sh && chm
 wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/SLDNS/install-sldns && chmod +x install-sldns && ./install-sldns
 # Informasi IP Saya dan Semua Port TCP UDP
 # wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/ipsaya.sh && chmod +x ipsaya.sh
-#
 # install xray sl-grpc
- wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/grpc/sl-grpc.sh && chmod +x sl-grpc.sh && screen -S sl-grpc ./sl-grpc.sh
+ #wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/grpc/sl-grpc.sh && chmod +x sl-grpc.sh && screen -S sl-grpc ./sl-grpc.sh
 # install xray grpc
- wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/grpc/xray-grpc.sh && chmod +x xray-grpc.sh && screen -S xray-grpc ./xray-grpc.sh
+ #wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/grpc/xray-grpc.sh && chmod +x xray-grpc.sh && screen -S xray-grpc ./xray-grpc.sh
 # install shadowsocks plugin
- wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/shadowsocks-plugin/install-ss-plugin.sh && chmod +x install-ss-plugin.sh && ./install-ss-plugin.sh
+ #wget https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/shadowsocks-plugin/install-ss-plugin.sh && chmod +x install-ss-plugin.sh && ./install-ss-plugin.sh
 
 
 rm -f /root/ssh-vpn.sh
