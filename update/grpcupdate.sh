c8e334a52b9ff3fca8e9cc8b675e68f0e24cb7af
hendra <120649258+Tarap-Kuhing@users.noreply.github.com>
2023-01-07 09:44:21 +0800

    Update grpcupdate.sh

--- update/grpcupdate.sh
+++ update/grpcupdate.sh
@@ -6,10 +6,10 @@ cd
 
 cd /usr/bin
 
-wget -O addgrpc "https://raw.githubusercontent.com/Trap-Kuhing/SCVPS/main/xray/addgrpc.sh"
-wget -O cekgrpc "https://raw.githubusercontent.com/Trap-Kuhing/SCVPS/main/xray/cekgrpc.sh"
-wget -O delgrpc "https://raw.githubusercontent.com/VTrap-Kuhing/SCVPS/main/xray/delgrpc.sh"
-wget -O renewgrpc "https://raw.githubusercontent.com/Trap-Kuhing/SCVPS/main/xray/renewgrpc.sh"
+wget -O addgrpc "https://raw.githubusercontent.com/Tarap-Kuhing/SCVPS/main/grpc/addgrpc.sh"
+wget -O cekgrpc "https://raw.githubusercontent.com/Trap-Kuhing/SCVPS/main/gprc/cekgrpc.sh"
+wget -O delgrpc "https://raw.githubusercontent.com/VTrap-Kuhing/SCVPS/main/gprc/delgrpc.sh"
+wget -O renewgrpc "https://raw.githubusercontent.com/Trap-Kuhing/SCVPS/main/gprc/renewgrpc.sh"
 
 
 chmod +x addgrpc
